a55f12a6a89618f750ea5664f1a64be32df3ba0e
Tom French <tom@tomfren.ch>
2020-01-23 20:31:38 +0000

    remove unused functions

--- client/src/lib/helpers.ts
+++ client/src/lib/helpers.ts
@@ -1,4 +1,4 @@
-import { getAddress, formatUnits } from 'ethers/utils';
+import { getAddress } from 'ethers/utils';
 
 function isValidAddress(str: string): boolean {
   try {
@@ -9,25 +9,9 @@ function isValidAddress(str: string): boolean {
   }
 }
 
-const convertToGWEI = (numberInWEI: string) => {
-  return Number(formatUnits(numberInWEI, 'gwei')).toString();
-};
-
-const convertToETH = (numberInWEI: string) => {
-  return Number(formatUnits(numberInWEI, 'ether'));
-};
-
-const convertFromGWEI = (numberInGWEI: string) => {
-  let numberGWEI: number = Number(numberInGWEI);
-  for (let i = 1; i < 10; i ++){
-    numberGWEI = Number(numberGWEI) * 10;
-  }
-  return String(numberGWEI);
-};
-
 const reduceAddress = (address: string) => {
   if (address.length < 10) return address
   return address.slice(0, 6) + '\u2026' + address.slice(-4)
 };
 
-export { isValidAddress, convertToGWEI, convertFromGWEI, convertToETH, reduceAddress };
+export { isValidAddress, reduceAddress };
